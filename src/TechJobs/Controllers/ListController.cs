d59f1d58d9adda402a1f8d997db0ba16b5fbdd79
dkamins314 <42924499+dkamins314@users.noreply.github.com>
2018-12-01 22:11:55 -0600

    final tech jobs

--- src/TechJobs/Controllers/ListController.cs
+++ src/TechJobs/Controllers/ListController.cs
@@ -22,37 +22,40 @@ namespace TechJobs.Controllers
         }
 
         public IActionResult Index()
-        {
-            ViewBag.columns = columnChoices;
+        {//displays job fields
+            ViewBag.columns = columnChoices;//all fields
             return View();
         }
 
         public IActionResult Values(string column)
         {
-            if (column.Equals("all"))
+            if (column.Equals("all"))//if request is to get all jobs
             {
-                List<Dictionary<string, string>> jobs = JobData.FindAll();
+                IEnumerable<Dictionary<string, string>> jobs = JobData.FindAll();
                 ViewBag.title =  "All Jobs";
                 ViewBag.jobs = jobs;
                 return View("Jobs");
             }
-            else
+            else // if request is to get jobs based on user input for a certain field
             {
-                List<string> items = JobData.FindAll(column);
+                IEnumerable<string> items = JobData.FindAll(column);//A list called "items" will contain
+                                                             // all matching properties found in that column
                 ViewBag.title =  "All " + columnChoices[column] + " Values";
-                ViewBag.column = column;
-                ViewBag.items = items;
+                ViewBag.column = column;//will pass field(column)title to display 
+                ViewBag.items = items;//will pass property(row) value to display 
                 return View();
             }
         }
 
         public IActionResult Jobs(string column, string value)
         {
-            List<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(column, value);
+            IEnumerable<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(column, value);
+            //dictionary "jobs" will hold job fields(columns) as keys and properties as values
             ViewBag.title = "Jobs with " + columnChoices[column] + ": " + value;
-            ViewBag.jobs = jobs;
+            //pass title  to be displayed "Jobs with (field): job value
+            ViewBag.jobs = jobs; //pass job value to display 
 
-            return View();
+            return View("Jobs");
         }
     }
 }
--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using TechJobs.Models;
 
 namespace TechJobs.Controllers
@@ -8,13 +9,39 @@ namespace TechJobs.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.columns = ListController.columnChoices;
-            ViewBag.title = "Search";
-            return View();
+            ViewBag.columns = ListController.columnChoices; //columns passed to ViewBag are 
+            //columns -ie fields from the column choices in the ListController
+            ViewBag.title = "Search"; //title to be displayed is "Search"
+            return View(); //Search: with fields for searching will be displayed
         }
 
-        // TODO #1 - Create a Results action method to process 
-        // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices; //all columns/fields are passed to ViewBag
+             
+            ViewBag.title = "Search";
+            if (string.IsNullOrEmpty(searchTerm))
+                          {
+                           return View("Index");
+                          }
+                          
+                          if (searchType.Equals("all"))
+                          {
+                           ViewBag.jobs = JobData.FindByValue(searchTerm);//JobData will be searched with
+                           //FindByValue method based on searchTerm -results will be passed into"jobs" variable in Viewbag
+                             
+                           return View("Index");
+                          }
+                          else
+                          {
+                              ViewBag.jobs = JobData.FindByColumnAndValue(searchType,searchTerm);
+                           
+                           return View("Index");
+                          }
 
+            // TODO #1 - Create a Results action method to process 
+            // search request and display results
+
+        }
     }
 }
